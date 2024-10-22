001d78fc690d7a5cd38116e8a33a556884670c1e
EmilyJaynee <emily.barrott@hotmail.co.uk>
2024-10-22 21:01:17 +0100

    Added all my JS into a function

--- JS/index.js
+++ JS/index.js
@@ -1,31 +1,35 @@
-let hebdenBridge = document.querySelector("#hebden-bridge");
-let hebdenBridgeDate = hebdenBridge.querySelector(".date");
-let hebdenBridgeTime = hebdenBridge.querySelector(".time");
-let hebdenBridgeTimeZone = moment().tz("Europe/London");
+function setCurrentTime(time) {
+  let hebdenBridge = document.querySelector("#hebden-bridge");
+  let hebdenBridgeDate = hebdenBridge.querySelector(".date");
+  let hebdenBridgeTime = hebdenBridge.querySelector(".time");
+  let hebdenBridgeTimeZone = moment().tz("Europe/London");
 
-hebdenBridgeDate.innerHTML = hebdenBridgeTimeZone.format("dddd Do MMMM");
-hebdenBridgeTime.innerHTML = hebdenBridgeTimeZone.format("HH:mm");
+  hebdenBridgeDate.innerHTML = hebdenBridgeTimeZone.format("dddd Do MMMM");
+  hebdenBridgeTime.innerHTML = hebdenBridgeTimeZone.format("HH:mm");
 
-let oahu = document.querySelector("#oahu");
-let oahuDate = oahu.querySelector(".date");
-let oahuTime = oahu.querySelector(".time");
-let oahuTimeZone = moment().tz("Pacific/Honolulu");
+  let oahu = document.querySelector("#oahu");
+  let oahuDate = oahu.querySelector(".date");
+  let oahuTime = oahu.querySelector(".time");
+  let oahuTimeZone = moment().tz("Pacific/Honolulu");
 
-oahuDate.innerHTML = oahuTimeZone.format("dddd Do MMMM");
-oahuTime.innerHTML = oahuTimeZone.format("HH:mm");
+  oahuDate.innerHTML = oahuTimeZone.format("dddd Do MMMM");
+  oahuTime.innerHTML = oahuTimeZone.format("HH:mm");
 
-let bangkok = document.querySelector("#bangkok");
-let bangkokDate = bangkok.querySelector(".date");
-let bangkokTime = bangkok.querySelector(".time");
-let bangkokTimeZone = moment().tz("Asia/Bangkok");
+  let bangkok = document.querySelector("#bangkok");
+  let bangkokDate = bangkok.querySelector(".date");
+  let bangkokTime = bangkok.querySelector(".time");
+  let bangkokTimeZone = moment().tz("Asia/Bangkok");
 
-bangkokDate.innerHTML = bangkokTimeZone.format("dddd Do MMMM");
-bangkokTime.innerHTML = bangkokTimeZone.format("HH:mm");
+  bangkokDate.innerHTML = bangkokTimeZone.format("dddd Do MMMM");
+  bangkokTime.innerHTML = bangkokTimeZone.format("HH:mm");
 
-let newOrleans = document.querySelector("#new-orleans");
-let newOrleansDate = newOrleans.querySelector(".date");
-let newOrleansTime = newOrleans.querySelector(".time");
-let newOrleansTimeZone = moment().tz("America/Matamoros");
+  let newOrleans = document.querySelector("#new-orleans");
+  let newOrleansDate = newOrleans.querySelector(".date");
+  let newOrleansTime = newOrleans.querySelector(".time");
+  let newOrleansTimeZone = moment().tz("America/Matamoros");
 
-newOrleansDate.innerHTML = newOrleansTimeZone.format("dddd Do MMMM");
-newOrleansTime.innerHTML = newOrleansTimeZone.format("HH:mm");
+  newOrleansDate.innerHTML = newOrleansTimeZone.format("dddd Do MMMM");
+  newOrleansTime.innerHTML = newOrleansTimeZone.format("HH:mm");
+}
+
+setCurrentTime();
