b1b9803c95b23fcbe933074970264182a0f1209e
EmilyJaynee <emily.barrott@hotmail.co.uk>
2024-10-22 20:31:47 +0100

    Used moment to add time and date for Hebden Bridge

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,7 @@
+let hebdenBridge = document.querySelector("#hebden-bridge");
+let hebdenBridgedate = hebdenBridge.querySelector(".date");
+let hebdenBridgeTime = hebdenBridge.querySelector(".time");
+let hebdenBridgeTimeZone = moment().tz("Europe/London");
 
+hebdenBridgedate.innerHTML = hebdenBridgeTimeZone.format("dddd Do MMMM");
+hebdenBridgeTime.innerHTML = hebdenBridgeTimeZone.format("HH:mm");
